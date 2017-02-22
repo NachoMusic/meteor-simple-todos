48be4bc5f89045606282ebf347044cf367738bea
NachoMusic <nachovaleraherreraes@gmail.com>
2017-02-22 20:18:44 +0100

    Added functionality. Public/private tasks. More secure code.

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -9,6 +9,8 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.subscribe('tasks');
+
     this.hideCompleted = false;
 
     this.helpers({
@@ -44,12 +46,7 @@ class TodosListCtrl {
   
   addTask(newTask) {
     // Insert a task into the collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', newTask);
  
     // Clear form
     this.newTask = '';
@@ -57,17 +54,16 @@ class TodosListCtrl {
 
   setChecked(task) {
     // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      },
-    });
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
  
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id);
   }
 
+  setPrivate(task) {
+    Meteor.call('tasks.setPrivate', task._id, !task.private);
+  }
 }
  
 export default angular.module('todosList', [
